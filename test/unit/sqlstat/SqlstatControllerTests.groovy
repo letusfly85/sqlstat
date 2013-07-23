48a0585b0a6c7c1231658cf30ab4a1672ee3c2ff
wada <wada.shunsuke@future.co.jp>
2013-07-23 18:18:11 +0900

    追加

---
+++ test/unit/sqlstat/SqlstatControllerTests.groovy
@@ -0,0 +1,17 @@
+package sqlstat
+
+import griffon.test.*
+
+class SqlstatControllerTests extends GriffonUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+        fail('Not implemented!')
+    }
+}
